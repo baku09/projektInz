85c2ec857e97d7f803a1332245b3245f45bb330e
Jakub Bąk <ktb.1999.ktb@gmail.com>
2024-06-20 17:41:48 +0200

    podstawy

---
+++ src/main/java/com/example/projektInz/ProjektInzApplication.java
@@ -0,0 +1,13 @@
+package com.example.projektInz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjektInzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjektInzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/projektInz/ProjektInzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projektInz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjektInzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
