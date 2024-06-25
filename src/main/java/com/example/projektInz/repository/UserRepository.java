478319894e5da24131a843d2898f074cc82b7766
Jakub Bąk <ktb.1999.ktb@gmail.com>
2024-06-25 17:45:15 +0200

    rejestrowanie i logowanie i testy

---
+++ src/main/java/com/example/projektInz/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.example.projektInz.repository;
+
+import com.example.projektInz.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
--- src/test/java/com/example/projektInz/ProjektInzApplicationTests.java
+++ src/test/java/com/example/projektInz/ProjektInzApplicationTests.java
@@ -1,13 +1,19 @@
 package com.example.projektInz;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-@SpringBootTest
+import java.util.Arrays;
+
+import static org.mockito.Mockito.when;
+
+
 class ProjektInzApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
